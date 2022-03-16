6001c70763847df389313e80fb718f3377c32c0c
Paul J <jorpaulvincent@gmail.com>
2022-03-16 21:50:29 +0800

    add view product api

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-app = FastAPI()
+
+class Credentials(BaseModel):
+    name: str = None
+    type: str = None
+    price: str = None
+
+
+app = FastAPI(title="Products", version="0.0.1")
 
 
 @app.get("/")
-def read_root():
+def home():
     return {"message": "Welcome to Paul Jor API"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/view")
+def view_product(product: Credentials):
+    return {
+        "Product Name:": product.name,
+        "Product Type:": product.type,
+        "Product Price:": product.price
+    }
